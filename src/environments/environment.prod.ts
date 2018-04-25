f6787d4e92f47ed8210377968d4b62ed19149981
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-04-24 22:33:54 -0300

    Resolvendo problemas de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyAHHXCFeVqid9fOpnt2sKHbJViTFMXGQ8o',
+    authDomain: 'd-assessment-online.firebaseapp.com',
+    databaseURL: 'https://d-assessment-online.firebaseio.com',
+    projectId: 'd-assessment-online',
+    storageBucket: 'd-assessment-online.appspot.com',
+    messagingSenderId: '874912498196'
+  },
+
   handlerUserUrl : 'https://us-central1-d-assessment-online.cloudfunctions.net/handlerUser'
 };
