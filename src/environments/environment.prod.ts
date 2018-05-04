c7845f7036bac5d44bead34c206217e62d1d0afa
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-05-04 14:04:35 -0300

    Definindo template WEB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,25 +1,25 @@
 export const environment = {
   production: true,
 
-  // firebase: {
-  //   apiKey: 'AIzaSyB8yIabxNk_4FgBqbdH2k87b24dX7T0LaI',
-  //   authDomain: 'assessment-online.firebaseapp.com',
-  //   databaseURL: 'https://assessment-online.firebaseio.com',
-  //   projectId: 'assessment-online',
-  //   storageBucket: 'assessment-online.appspot.com',
-  //   messagingSenderId: '658372006724'
-  // },
-  //
-  // handlerUserUrl : 'https://us-central1-assessment-online.cloudfunctions.net/handlerUser',
-
   firebase: {
-    apiKey: 'AIzaSyAHHXCFeVqid9fOpnt2sKHbJViTFMXGQ8o',
-    authDomain: 'd-assessment-online.firebaseapp.com',
-    databaseURL: 'https://d-assessment-online.firebaseio.com',
-    projectId: 'd-assessment-online',
-    storageBucket: 'd-assessment-online.appspot.com',
-    messagingSenderId: '874912498196'
+    apiKey: 'AIzaSyBEAZr-4h4hc9p_X1grFu52gMHbt2LhtOY',
+    authDomain: 'cinecataratas-online.firebaseapp.com',
+    databaseURL: 'https://cinecataratas-online.firebaseio.com',
+    projectId: 'cinecataratas-online',
+    storageBucket: 'cinecataratas-online.appspot.com',
+    messagingSenderId: '450535726461'
   },
 
-  handlerUserUrl : 'https://us-central1-d-assessment-online.cloudfunctions.net/handlerUser'
+  handlerUserUrl : 'https://us-central1-cinecataratas-online.cloudfunctions.net/handlerUser',
+
+  // firebase: {
+  //   apiKey: 'AIzaSyAHHXCFeVqid9fOpnt2sKHbJViTFMXGQ8o',
+  //   authDomain: 'd-assessment-online.firebaseapp.com',
+  //   databaseURL: 'https://d-assessment-online.firebaseio.com',
+  //   projectId: 'd-assessment-online',
+  //   storageBucket: 'd-assessment-online.appspot.com',
+  //   messagingSenderId: '874912498196'
+  // },
+  //
+  // handlerUserUrl : 'https://us-central1-d-assessment-online.cloudfunctions.net/handlerUser'
 };
