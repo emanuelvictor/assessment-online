0346878d623a707a66656255a3d8bba12c8f07e2
emanuelvictor <emanuelvictor@msn.com>
2018-09-29 21:40:39 -0300

    Feito SQL de migraçãod ec onfiguração

---
+++ back-end/src/main/java/br/com/assessment/domain/resource/ConfiguracaoResource.java
@@ -0,0 +1,32 @@
+package br.com.assessment.domain.resource;
+
+import br.com.assessment.domain.entity.usuario.Perfil;
+import br.com.assessment.domain.service.ContaService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/contas")
+@AllArgsConstructor
+public class ContaResource {
+
+    private final ContaService contaService;
+
+    /**
+     * TODO verificar se não da pra unificar com o usuário
+     * @param email {String}
+     * @return Mono<UserDetails>
+     */
+    @GetMapping(params = "email")
+    @PreAuthorize("hasAnyAuthority('" + Perfil.ATENDENTE_VALUE + "')")
+    public Mono<UserDetails> findUsuarioByEmail(@RequestParam final String email) {
+        return this.contaService.findUsuarioByEmail(email);
+    }
+
+}
---
+++ back-end/src/main/java/br/com/assessment/domain/service/ConfiguracaoService.java
@@ -0,0 +1,65 @@
+package br.com.assessment.domain.service;
+
+import br.com.assessment.application.context.Context;
+import br.com.assessment.domain.entity.unidade.Unidade;
+import br.com.assessment.domain.entity.usuario.Perfil;
+import br.com.assessment.domain.entity.usuario.Usuario;
+import br.com.assessment.domain.repository.ContaRepository;
+import br.com.assessment.domain.repository.UnidadeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UnidadeService {
+
+    private final UnidadeRepository unidadeRepository;
+
+    private final ContaRepository contaRepository;
+
+    public Unidade save(final long id, final Unidade unidade) {
+        Assert.isTrue(unidade.getId() != null && unidade.getId().equals(id), "Você não tem acesso a essa unidade"); //TODO colocar validator em uma service, e colocar na camada de cima
+        return this.save(unidade);
+    }
+
+    public Unidade save(final Unidade unidade) {
+        return this.unidadeRepository.save(unidade);
+    }
+
+    public void delete(final long id) {
+        this.unidadeRepository.deleteById(id);
+    }
+
+    public Optional<Unidade> findById(final long id) {
+        return this.unidadeRepository.findById(id);
+    }
+
+    public Page<Unidade> listByFilters(final String defaultFilter,
+                                       final String enderecoFilter,
+                                       final LocalDateTime dataInicioFilter,
+                                       final LocalDateTime dataTerminoFilter,
+                                       final Pageable pageable) {
+
+        final Usuario usuario = contaRepository.findByEmailIgnoreCase(Context.getCurrentUsername()).getUsuario();
+
+        return this.unidadeRepository.listByFilters(
+                usuario.getId(),
+                usuario.getConta().getPerfil().name(),
+                defaultFilter,
+                enderecoFilter,
+                dataInicioFilter,
+                dataTerminoFilter,
+                pageable);
+
+    }
+}
