1443fa118b704be171b1053eec7fd9db088208cc
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-11-01 17:15:06 -0200

    Implementado estatísticas

--- front-end/src/web/domain/presentation/dashboard/unidade/visualizar-unidade/estatisticas/estatisticas-unidade.component.ts
+++ front-end/src/web/domain/presentation/dashboard/unidade/visualizar-unidade/estatisticas/estatisticas-unidade.component.ts
@@ -1,12 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
-import {TdDigitsPipe} from '@covalent/core';
 import {ActivatedRoute} from '@angular/router';
-import {AvaliacaoService} from '../../../../../service/avaliacao.service';
 import {textMasks} from '../../../../controls/text-masks/text-masks';
-import * as moment from 'moment';
 import {UnidadeService} from '../../../../../service/unidade.service';
-import {Avaliacao} from '../../../../../entity/avaliacao/avaliacao.model';
+import {Configuracao} from "../../../../../entity/configuracao/configuracao.model";
+import {Unidade} from "../../../../../entity/unidade/unidade.model";
+import {ConfiguracaoService} from "../../../../../service/configuracao.service";
 
 @Component({
   selector: 'estatisticas-unidade',
@@ -48,11 +47,10 @@ export class EstatisticasUnidadeComponent implements OnInit {
     }
   ];
 
-
-  // options
-  showLegend = false;
-  yAxisLabel = 'Avaliações';
-
+  /**
+   *
+   * @type {{domain: [string , string , string , string , string]}}
+   */
   colorScheme: any = {
     domain: [
       '#c21c1f',
@@ -64,28 +62,13 @@ export class EstatisticasUnidadeComponent implements OnInit {
   };
 
   /**
-   *
-   */
-  avaliacoes: Avaliacao[];
-
-  /**
    * Armazena o nome do rankeavel
    */
   rankeavel: any;
 
   /**
    *
-   * @type {number}
    */
-  avaliacoes1 = 0;
-  avaliacoes2 = 0;
-  avaliacoes3 = 0;
-  avaliacoes4 = 0;
-  avaliacoes5 = 0;
-
-  public dataInicio;
-  public dataFim;
-
   mapper: any = [
     {
       'name': 'Avaliações',
@@ -94,22 +77,39 @@ export class EstatisticasUnidadeComponent implements OnInit {
   ];
 
   /**
-   * Informa se há avaliações a serem exibidas
-   * @type {boolean}
+   *
    */
-  loading = true;
+  public pageRequest = { // PageRequest
+    size: 20,
+    page: 0,
+    sort: null,
+    defaultFilter: [],
+    unidadesFilter: [],
+    dataInicioFilter: null,
+    dataTerminoFilter: null
+  };
+
+  /**
+   *
+   */
+  unidades: Unidade[];
+
+  /**
+   *
+   */
+  configuracao: Configuracao;
 
   /**
    *
    * @param {Title} title
-   * @param {UnidadeService} unidadeService
+   * @param {UnidadeService} usuarioService
    * @param {ActivatedRoute} activatedRoute
-   * @param {AvaliacaoService} avaliacaoService
+   * @param {ConfiguracaoService} configuracaoService
    */
   constructor(private title: Title,
-              private unidadeService: UnidadeService,
+              private usuarioService: UnidadeService,
               private activatedRoute: ActivatedRoute,
-              private avaliacaoService: AvaliacaoService) {
+              private configuracaoService: ConfiguracaoService) {
   }
 
   /**
@@ -117,114 +117,46 @@ export class EstatisticasUnidadeComponent implements OnInit {
    */
   ngOnInit() {
 
-    this.unidadeService.findById(this.activatedRoute.snapshot.params['id'])
-      .subscribe((rankeavel: any) => {
-        this.rankeavel = rankeavel;
-        this.title.setTitle('Estatisticas de ' + this.rankeavel.nome);
+    this.configuracaoService.configuracao.subscribe(configuracao => {
+
+      this.configuracao = configuracao;
+
+      this.usuarioService.findById(this.activatedRoute.snapshot.params['id']).subscribe((rankeavel: any) => {
+
+        this.title.setTitle('Estatisticas de ' + rankeavel.nome);
+
+        this.pageRequest.defaultFilter = [rankeavel.nome];
+
+        this.usuarioService.listByFilters(this.pageRequest).subscribe(result => {
+
+          this.rankeavel = result.content[0];
+
+          this.mapEstatisticas();
+
+        });
+
       });
 
-    this.listAll();
-  }
-
-  /**
-   *
-   */
-  initAvaliacoes() {
-    this.avaliacoes1 = 0;
-    this.avaliacoes2 = 0;
-    this.avaliacoes3 = 0;
-    this.avaliacoes4 = 0;
-    this.avaliacoes5 = 0;
-  }
-
-  /**
-   *
-   */
-  public listAll() {
-
-    this.loading = true;
-
-    /**
-     * Filtra as avaliações
-     */
-    this.avaliacaoService.listAvaliacoesByUnidadeKey(this.activatedRoute.snapshot.params['key'])
-      .subscribe(avaliacoes => {
-        this.avaliacoes = avaliacoes;
-        this.listEstatisticasByDates(this.dataInicio, this.dataFim);
-      })
-  }
-
-  /**
-   *
-   * @param dataInicio
-   * @param dataFim
-   */
-  public listEstatisticasByDates(dataInicio, dataFim) {
-
-    /**
-     * Zera as avaliações
-     */
-    this.initAvaliacoes();
-
-    /**
-     * Encerra loading inicial
-     */
-    this.loading = false;
-
-    /**
-     * Trada as datas, setando meia noite na data de início, e 23:59 na data de fim.
-     * @type {moment.Moment}
-     */
-    const dataInicioAux = dataInicio ? moment(dataInicio, 'DD/MM/YYYY').hour(0).minute(0) : null;
-    const dataFimAux = dataFim ? moment(dataFim, 'DD/MM/YYYY').hour(23).minute(59) : null;
-
-    this.avaliacoes.forEach(avaliacao => {
-
-      if (
-        (!dataFimAux || moment(new Date(avaliacao.data), 'DD/MM/YYYY').isBefore(moment(dataFimAux, 'DD/MM/YYYY')))
-        && (!dataInicioAux || moment(new Date(avaliacao.data), 'DD/MM/YYYY').isAfter(moment(dataInicioAux, 'DD/MM/YYYY')))
-      ) {
-
-        if (avaliacao.nota === 1) {
-          this.avaliacoes1 = this.avaliacoes1 + 1;
-        }
-        if (avaliacao.nota === 2) {
-          this.avaliacoes2 = this.avaliacoes2 + 1;
-        }
-        if (avaliacao.nota === 3) {
-          this.avaliacoes3 = this.avaliacoes3 + 1;
-        }
-        if (avaliacao.nota === 4) {
-          this.avaliacoes4 = this.avaliacoes4 + 1;
-        }
-        if (avaliacao.nota === 5) {
-          this.avaliacoes5 = this.avaliacoes5 + 1;
-        }
-      }
     });
+
+  }
+
+  /**
+   *
+   */
+  public mapEstatisticas() {
 
     /**
      * Falcatrua
      */
     this.multi = this.mapper.map((group: any) => {
-      group.series[0] = {value: this.avaliacoes1, name: 'Terrível'};
-      group.series[1] = {value: this.avaliacoes2, name: 'Ruim'};
-      group.series[2] = {value: this.avaliacoes3, name: 'Meia boca'};
-      group.series[3] = {value: this.avaliacoes4, name: 'Bacana'};
-      group.series[4] = {value: this.avaliacoes5, name: 'Top da balada'};
+      group.series[0] = {value: this.rankeavel.avaliacoes1, name: this.configuracao.um};
+      group.series[1] = {value: this.rankeavel.avaliacoes2, name: this.configuracao.dois};
+      group.series[2] = {value: this.rankeavel.avaliacoes3, name: this.configuracao.tres};
+      group.series[3] = {value: this.rankeavel.avaliacoes4, name: this.configuracao.quatro};
+      group.series[4] = {value: this.rankeavel.avaliacoes5, name: this.configuracao.cinco};
       return group;
     });
   }
 
-  // ngx transform using covalent digits pipe
-  axisDigits(val: any): any {
-    return new TdDigitsPipe().transform(val);
-  }
 }
-
-export let multi: any = [
-  {
-    'name': 'Avaliações',
-    'series': [],
-  },
-];
