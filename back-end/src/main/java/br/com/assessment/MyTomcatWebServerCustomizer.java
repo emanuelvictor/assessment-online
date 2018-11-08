888bc31da73a1e6971df5843f0549d6c8ee3c428
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-11-08 14:33:49 -0200

    retrocedendo vai

--- back-end/src/main/java/br/com/assessment/MyTomcatWebServerCustomizer.java
+++ back-end/src/main/java/br/com/assessment/MyTomcatWebServerCustomizer.java
@@ -1,10 +1,10 @@
 package br.com.assessment;
 
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.stereotype.Component;
+//import org.apache.catalina.connector.Connector;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.stereotype.Component;
 
 //@Component
 public class MyTomcatWebServerCustomizer
