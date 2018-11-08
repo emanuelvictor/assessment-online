9d68a861549ec8e9f3f9d5f06d31bc3965729a5a
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-11-08 14:12:30 -0200

    vai

---
+++ back-end/src/main/java/br/com/assessment/MyTomcatWebServerCustomizer.java
@@ -0,0 +1,22 @@
+package br.com.assessment;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Value("${server.http.port}") int httpPort;
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+        // customize the factory here
+        factory.addAdditionalTomcatConnectors(connector);
+    }
+}
+
