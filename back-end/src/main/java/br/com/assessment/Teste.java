2a2006b695b38b2cfd64a14907a98986cd8495ad
Emanuel Victor de Oliveira Fonseca <emanuel.fonseca@pti.org.br>
2018-11-20 10:21:49 -0200

    Testando java 8

---
+++ back-end/src/main/java/br/com/assessment/Teste.java
@@ -0,0 +1,16 @@
+package br.com.assessment;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        // Exemplo 1
+        com.emanuelvictor.exemplo1.Pessoa pessoa = new com.emanuelvictor.exemplo1.Pessoa();
+        pessoa.setId(1);
+        pessoa.setNome("Jesus");
+
+        System.out.println("\n Id .....:"  + pessoa.getId());
+        System.out.println("\n Nome ...:"  + pessoa.getNome());
+    }
+
+}
