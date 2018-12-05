eaee6fcc87c69426109350a71a28d44b085798a3
emanuelvictor <emanuelvictor@msn.com>
2018-12-05 15:15:20 -0200

    Alterações em SQL

--- back-end/src/main/resources/db/migration/USUARIOS.sql
+++ back-end/src/main/resources/db/migration/USUARIOS.sql
@@ -5,4 +5,4 @@ INSERT INTO usuario (id)
   VALUES (1);
 
 INSERT INTO conta (created, email, esquema, administrador, root, password, usuario_id)
-  VALUES (NOW(),'admin@admin.com', 'public', true, true , '$2a$10$ZpR4ethoJ1Ne4Qn42OSX5euVeGi3tkrm4UwO6MlwZ5xCeOAoVB25m', 1);
+  VALUES (NOW(),'admin@assessment-online.com.br', 'public', true, true, '$2a$10$ZpR4ethoJ1Ne4Qn42OSX5euVeGi3tkrm4UwO6MlwZ5xCeOAoVB25m', 1);
