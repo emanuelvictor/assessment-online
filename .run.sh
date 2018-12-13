4a51053748e864e433f64ac0b65bb1a16b70fce8
emanuelvictor <emanuelvictor@msn.com>
2018-12-13 21:10:50 -0200

    Adicionando configurações de memória

--- .run.sh
+++ .run.sh
@@ -1,4 +1,4 @@
 cd front-end
 npm install && npm run build
 cd ../back-end
-mvn spring-boot:run -Dspring-boot.run.profiles=production &
+mvn spring-boot:run -Drun.jvmArguments="-Xmx2048m -Xms2048m"  -Dspring-boot.run.profiles=production &
