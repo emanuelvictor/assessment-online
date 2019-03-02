5c07ef86b07abfa18973dc93c7d5b9f6fea12ef5
emanuelvictor <emanuelvictor@msn.com>
2019-03-02 00:58:09 -0300

    Classe para teste de data

---
+++ back-end/src/main/java/br/com/ubest/TesteData.java
@@ -0,0 +1,11 @@
+package br.com.ubest;
+
+import java.time.LocalDateTime;
+
+public class TesteData {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+
+}
