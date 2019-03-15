593e34451fc42301c6ffab8d1b928a0bc9187118
Emanuel Victor <emanuelvictor@msn.com>
2019-03-15 18:29:25 -0300

    Implementando script de migração

--- back-end/src/main/resources/db/migration/v1.0.4__RELEASE.sql
+++ back-end/src/main/resources/db/migration/v1.0.4__RELEASE.sql
@@ -1,3 +1,9 @@
+SET search_path = "public";
+
+
+
+SET search_path = "teste@teste.com";
+
 ALTER TABLE conta DROP COLUMN usuario_id;
 
 ALTER TABLE conta_aud DROP COLUMN usuario_id;
@@ -13,7 +19,6 @@ ALTER TABLE ONLY pessoa
   ADD CONSTRAINT fkm9ew1jp3jewapkiridletuerr FOREIGN KEY (conta_id) REFERENCES public.conta(id);
 
 ALTER TABLE pessoa_aud ADD COLUMN conta_id BIGINT;
-
 
 
 
@@ -34,8 +39,22 @@ ALTER TABLE ONLY operador
   DROP CONSTRAINT fkajlncx53fcqxjpepj4dsogdel;
 
 
+CREATE OR REPLACE FUNCTION funcaoExemplo() RETURNS void LANGUAGE plpgsql AS $$
+DECLARE
+  local_id_pessoa integer;
+  tuplas_da_consulta CURSOR IS SELECT * FROM pessoa;
+BEGIN
+  FOR uma_tupla IN tuplas_da_consulta LOOP
+    local_id_pessoa = nextval('public.pessoa_id_seq'::regclass);
+    INSERT INTO public.pessoa(id, created, updated, documento, nome, conta_id) VALUES (local_id_pessoa , uma_tupla.created , uma_tupla.updated , uma_tupla.documento , uma_tupla.nome , uma_tupla.conta_id );
+    UPDATE public.usuario SET id = local_id_pessoa WHERE id = uma_tupla.id;
+    UPDATE public.conta SET pessoa_id = local_id_pessoa WHERE id = uma_tupla.conta_id;
+    UPDATE avaliavel SET usuario_id = local_id_pessoa WHERE usuario_id = uma_tupla.id;
+    UPDATE operador SET usuario_id = local_id_pessoa WHERE usuario_id = uma_tupla.id;
+  END LOOP;
+END; $$;
 
-
+SELECT funcaoExemplo();
 
 -- Adiciona novamente a CONSTRAINT de public.pessoa_id na tabela de usuário
 ALTER TABLE ONLY usuario
@@ -52,3 +71,12 @@ ALTER TABLE ONLY usuario_aud
 ALTER TABLE usuario DROP COLUMN conta_id;
 
 ALTER TABLE usuario_aud DROP COLUMN conta_id;
+
+
+-- Dropa a CONSTRAINT da avaliavel_id (somente para recriar apontando para o public)
+ALTER TABLE ONLY avaliavel
+  ADD CONSTRAINT fkp84hu90r7k1p8d7e4rt3vprsl FOREIGN KEY (usuario_id) REFERENCES usuario(id);
+
+-- Dropa a CONSTRAINT da operador_id (somente para recriar apontando para o public)
+ALTER TABLE ONLY operador
+  ADD CONSTRAINT fkajlncx53fcqxjpepj4dsogdel FOREIGN KEY (usuario_id) REFERENCES usuario(id);
