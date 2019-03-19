5ebc2fe582c62702567d138f29c5009a6abb4c8c
Emanuel Victor <emanuelvictor@msn.com>
2019-03-19 14:11:29 -0300

    Alteração em arquivo de build

--- .run.sh
+++ .run.sh
@@ -1,4 +1,4 @@
 cd front-end
 npm install && npm run build
 cd ../back-end
-mvn spring-boot:run -Drun.jvmArguments="-Xmx2048m -Xms2048m"  -Dspring-boot.run.profiles=production &
+mvn spring-boot:run -Drun.jvmArguments="-Xmx2048m -Xms2048m --add-modules java.se --add-exports java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"  -Dspring-boot.run.profiles=RELEASE &
