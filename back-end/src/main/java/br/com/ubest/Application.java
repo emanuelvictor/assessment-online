23b27a337e3d72610264172f34d05259550747b5
Emanuel Victor <emanuelvictor@msn.com>
2019-03-25 11:40:13 -0300

    Adicionando undertow

--- back-end/src/main/java/br/com/ubest/Application.java
+++ back-end/src/main/java/br/com/ubest/Application.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +26,7 @@ import java.util.Arrays;
 
 @EnableAsync
 @SpringBootApplication
-public class Application {
+public class Application  extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
@@ -38,6 +40,14 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     *
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     /**
