b04c1c16907037911f9d82756812e28b7fe0c10a
Emanuel Victor <emanuel.fonseca@pti.org.br>
2019-08-20 17:25:33 -0300

    Websocket do back-end funcionando

--- back-end/src/main/java/br/com/ubest/application/multitenancy/TenantIdentifierResolver.java
+++ back-end/src/main/java/br/com/ubest/application/multitenancy/TenantIdentifierResolver.java
@@ -1,46 +1,25 @@
 package br.com.ubest.application.multitenancy;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 import static br.com.ubest.Application.DEFAULT_TENANT_ID;
-import static br.com.ubest.Application.SCHEMA_NAME;
 import static br.com.ubest.infrastructure.suport.Utils.removeNoCache;
 
 @Component
 @RequiredArgsConstructor
-@Transactional
-public class TenantIdentifierResolver
-//        implements CurrentTenantIdentifierResolver
-{
+public class TenantIdentifierResolver {
 
     private String username;
 
     private String schema = DEFAULT_TENANT_ID;
 
-//    private final EntityManager entityManager;
-
-//    @Override
     public String resolveCurrentTenantIdentifier() {
         return schema;
     }
 
-//    @Override
-    public boolean validateExistingCurrentSessions() {
-        return true;
-    }
-
     public void setSchema(final String schema) {
         this.schema = removeNoCache(schema);
-
-//        org.hibernate.Filter filter = entityManager.unwrap(Session.class).enableFilter("tenantFilter");
-//        filter.setParameter("tenant", this.schema);
-//        filter.validate();
     }
 
     public void setUsername(final String username) {
