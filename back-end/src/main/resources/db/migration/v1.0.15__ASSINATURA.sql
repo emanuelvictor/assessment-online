5a8bc7d92de9bd37d94933d24e2832b9284ef88b
Emanuel Victor <emanuel.fonseca@pti.org.br>
2019-09-04 08:22:06 -0300

    Implementando sql de migração de assinaturas

--- back-end/src/main/resources/db/migration/v1.0.15__ASSINATURA.sql
+++ back-end/src/main/resources/db/migration/v1.0.15__ASSINATURA.sql
@@ -1,39 +1,39 @@
 create table if not exists assinatura
 (
-    id                            bigserial    not null,
-    created                       timestamp    not null,
-    updated                       timestamp,
+    id                             bigserial    not null,
+    created                        timestamp    not null,
+    updated                        timestamp,
     primary key (id),
-    tenant                        varchar(150) not null,
-    mes_validade                  varchar(20),
-    ano_validade                  varchar(50),
-    data_vencimento               timestamp,
-    numero_cartao                 bigint,
-    nome_titular_cartao           varchar(150),
-    documento_titular_cartao      varchar(20),
-    dataNascimento_titular_cartao timestamp,
-    codigo_area                   smallint,
-    telefone                      bigint,
-    endereco_id                   bigint
+    tenant                         varchar(150) not null,
+    mes_validade                   varchar(20),
+    ano_validade                   varchar(50),
+    data_vencimento                timestamp,
+    numero_cartao                  bigint,
+    nome_titular_cartao            varchar(150),
+    documento_titular_cartao       varchar(20),
+    data_nascimento_titular_cartao timestamp,
+    codigo_area                    smallint,
+    telefone                       bigint,
+    endereco_id                    bigint
 );
 
 create table if not exists assinatura_aud
 (
-    id                            int8 not null,
-    rev                           int8 not null,
-    revtype                       int2,
+    id                             int8 not null,
+    rev                            int8 not null,
+    revtype                        int2,
     primary key (id, rev),
-    tenant                        varchar(150),
-    mes_validade                  varchar(20),
-    ano_validade                  varchar(50),
-    data_vencimento               timestamp,
-    numero_cartao                 bigint,
-    nome_titular_cartao           varchar(150),
-    documento_titular_cartao      varchar(20),
-    dataNascimento_titular_cartao timestamp,
-    codigo_area                   smallint,
-    telefone                      bigint,
-    endereco_id                   bigint
+    tenant                         varchar(150),
+    mes_validade                   varchar(20),
+    ano_validade                   varchar(50),
+    data_vencimento                timestamp,
+    numero_cartao                  bigint,
+    nome_titular_cartao            varchar(150),
+    documento_titular_cartao       varchar(20),
+    data_nascimento_titular_cartao timestamp,
+    codigo_area                    smallint,
+    telefone                       bigint,
+    endereco_id                    bigint
 );
 
 alter table assinatura
@@ -42,28 +42,17 @@ alter table assinatura
     add constraint UK_numero_cartao unique (numero_cartao);
 
 -- Inserir assinatura
-INSERT INTO public.assinatura (created, tenant) VALUES (NOW(), current_schema());
+INSERT INTO public.assinatura (created, tenant)
+VALUES (NOW(), current_schema());
 
 -- atualizar licencas
-alter table licenca add column assinatura_id bigint;
-alter table licenca_aud add column assinatura_id bigint;
+alter table licenca
+    add column assinatura_id bigint;
+alter table licenca_aud
+    add column assinatura_id bigint;
 
-update licenca set assinatura_id = (SELECT MAX(id) FROM public.assinatura WHERE tenant = "current_schema"() );
+update licenca
+set assinatura_id = (SELECT MAX(id) FROM public.assinatura WHERE tenant = "current_schema"());
 
-alter table licenca alter column assinatura_id set not null;
--- not null default MAX((SELECT MAX(id) FROM publica.assinatura WHERE tenant = "current_schema"() ));
-
--- create table licenca_aud (id int8 not null, rev int8 not null, revtype int2, nome varchar(150), numero bigint, primary key (id, rev));
--- create table unidade_tipo_avaliacao_licenca_aud (id int8 not null, rev int8 not null, revtype int2, ativo boolean, ordem int2, licenca_id int8, unidade_tipo_avaliacao_id int8, primary key (id, rev));
--- create table unidade_tipo_avaliacao_licenca (id  bigserial not null, created timestamp not null, updated timestamp, ativo boolean not null, ordem int2, licenca_id int8 not null, unidade_tipo_avaliacao_id int8 not null, primary key (id));
--- alter table licenca drop constraint if exists UK_tcudt9flboi41o17wagkwx8tw;
--- alter table licenca add constraint UK_tcudt9flboi41o17wagkwx8tw unique (numero);
--- alter table unidade_tipo_avaliacao_licenca drop constraint if exists UKli1jamrtf9006goepkinibpo2;
--- alter table unidade_tipo_avaliacao_licenca add constraint UKli1jamrtf9006goepkinibpo2 unique (unidade_tipo_avaliacao_id, licenca_id);
--- -- alter table unidade_tipo_avaliacao_licenca drop constraint if exists UKcdje95t2heav6arkqvwwjxm5c;
--- --
--- -- alter table unidade_tipo_avaliacao_licenca add constraint UKcdje95t2heav6arkqvwwjxm5c unique (licenca_id, ordem);
--- alter table licenca_aud add constraint FKea59wma882p0pwak1gc939lfa foreign key (rev) references public.revision;
--- alter table unidade_tipo_avaliacao_licenca_aud add constraint FK9054nel78bl424lyvxt78byep foreign key (rev) references public.revision;
--- alter table unidade_tipo_avaliacao_licenca add constraint FKl9hea59r09t9mewuo74l1yqj4 foreign key (licenca_id) references public.licenca;
--- alter table unidade_tipo_avaliacao_licenca add constraint FK5vxq5vkng3gjeo0bh644ejnuq foreign key (unidade_tipo_avaliacao_id) references unidade_tipo_avaliacao;
+-- alter table licenca
+--     alter column assinatura_id set not null;
