a249e823f2f1551975fef44e1b825e39b0b8186e
Emanuel Victor <emanuel.fonseca@pti.org.br>
2019-10-03 13:05:53 -0300

    ref #128

--- back-end/src/main/java/br/com/ubest/application/websocket/DispositivoWebSocketHandler.java
+++ back-end/src/main/java/br/com/ubest/application/websocket/DispositivoWebSocketHandler.java
@@ -13,9 +13,9 @@ import java.util.List;
 public class DispositivoWebSocketHandler extends GenericWebSocketHandler<Dispositivo> {
 
     DispositivoWebSocketHandler(final JpaRepository<Dispositivo, Long> jpaRepository,
-                            final JsonConverter<Dispositivo> jsonConverter,
-                            final String path, final String numero,
-                            final List<WrapperHandler<Dispositivo>> wrappersHandler) {
+                                final JsonConverter<Dispositivo> jsonConverter,
+                                final String path, final String numero,
+                                final List<WrapperHandler<Dispositivo>> wrappersHandler) {
         super(jpaRepository, jsonConverter, path, numero, wrappersHandler);
     }
 
@@ -51,9 +51,7 @@ public class DispositivoWebSocketHandler extends GenericWebSocketHandler<Disposi
                 );
 
         // Envia o que está na base
-        return session.send(wrapperHandler.getOutputMessages().map(s -> {
-            return session.textMessage(this.jsonConverter.toJSON(((DispositivoRepository) jpaRepository).findByNumeroLicenca(numero).orElse(new Dispositivo())));
-        }));
+        return session.send(wrapperHandler.getOutputMessages().map(s -> session.textMessage(this.jsonConverter.toJSON(((DispositivoRepository) jpaRepository).findByNumeroLicenca(numero).orElse(new Dispositivo())))));
     }
 
     /**
@@ -67,3 +65,71 @@ public class DispositivoWebSocketHandler extends GenericWebSocketHandler<Disposi
     }
 
 }
+
+
+
+
+//package br.com.ubest.application.websocket;
+//
+//import br.com.ubest.application.converters.JsonConverter;
+//import br.com.ubest.domain.entity.unidade.Dispositivo;
+//import br.com.ubest.domain.repository.DispositivoRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.web.reactive.socket.WebSocketMessage;
+//import org.springframework.web.reactive.socket.WebSocketSession;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.List;
+//
+//public class DispositivoWebSocketHandler extends GenericWebSocketHandler<Dispositivo> {
+//
+//    DispositivoWebSocketHandler(final JpaRepository<Dispositivo, Long> jpaRepository,
+//                                final JsonConverter<Dispositivo> jsonConverter,
+//                                final String path, final String numero,
+//                                final List<WrapperHandler<Dispositivo>> wrappersHandler) {
+//        super(jpaRepository, jsonConverter, path, numero, wrappersHandler);
+//    }
+//
+//    @Override
+//    public Mono<Void> handle(final WebSocketSession session) {
+//
+//        final var numero = (Long) this.extractParameterNameFromSession(session);
+//
+//        final WrapperHandler<Dispositivo> wrapperHandler = this.getWrapperHandlerByRosourceId(numero);
+//
+//        final WebSocketMessageSubscriber<Dispositivo> subscriber = new WebSocketMessageSubscriber<>(wrapperHandler.getMessagePublisher());
+//
+//        session.receive()
+//                .map(WebSocketMessage::getPayloadAsText)
+//                // Recebendo salva
+//                .map(s -> {
+//                    final Dispositivo dispositivo = jsonConverter.toObject(s);
+//                    dispositivo.setNumeroLicenca((Long) this.extractParameterNameFromSession(session));
+//
+//                    final Dispositivo dispositivoDB = ((DispositivoRepository) jpaRepository).findByNumeroLicenca(numero).orElse(new Dispositivo());
+//                    dispositivo.setId(dispositivoDB.getId());
+//
+//                    dispositivo.setUnidadesTiposAvaliacoesDispositivo(dispositivoDB.getUnidadesTiposAvaliacoesDispositivo());
+//
+//                    return this.jpaRepository.save(dispositivo);
+//                })
+//                .subscribe(subscriber::onNext,
+//                        throwable -> subscriber.onError(throwable, wrapperHandler, wrappersHandler, numero),
+//                        () -> subscriber.onComplete(wrapperHandler, wrappersHandler, numero)
+//                );
+//
+//        // Envia o que está na base
+//        return session.send(wrapperHandler.getOutputMessages().map(s -> session.textMessage(this.jsonConverter.toJSON(((DispositivoRepository) jpaRepository).findByNumeroLicenca(numero).orElse(new Dispositivo())))));
+//    }
+//
+//    /**
+//     * @param resourceId long
+//     * @return WrapperHandler<Dispositivo>
+//     */
+//    private WrapperHandler<Dispositivo> getWrapperHandlerByRosourceId(final Object resourceId) {
+//        if (wrappersHandler.stream().noneMatch(wh -> wh.getResourceId().equals(resourceId)))
+//            wrappersHandler.add(new WrapperHandler<>(((DispositivoRepository) this.jpaRepository).findByNumeroLicenca((Long) resourceId), jsonConverter, resourceId));
+//        return wrappersHandler.stream().filter(wh -> wh.getResourceId().equals(resourceId)).findFirst().orElseThrow();
+//    }
+//
+//}
--- back-end/src/main/java/br/com/ubest/application/websocket/WebSocketMessageSubscriber.java
+++ back-end/src/main/java/br/com/ubest/application/websocket/WebSocketMessageSubscriber.java
@@ -2,29 +2,67 @@ package br.com.ubest.application.websocket;
 
 import reactor.core.publisher.UnicastProcessor;
 
-import java.util.Optional;
+import java.util.List;
 
 public class WebSocketMessageSubscriber<T> {
+
     private UnicastProcessor<T> messagePublisher;
-    private Optional<T> lastReceivedMessage = Optional.empty();
 
     WebSocketMessageSubscriber(final UnicastProcessor<T> messagePublisher) {
         this.messagePublisher = messagePublisher;
     }
 
     void onNext(final T object) {
-        lastReceivedMessage = Optional.of(object);
         messagePublisher.onNext(object);
     }
 
     void onError(final Throwable error) {
         error.printStackTrace();
-//        messagePublisher.onError(error);
+        messagePublisher.onError(error);
+//        wrapperHandler.decrementSubscribers();
+//        if (wrapperHandler.nonSubscribers())
+//            wrappersHandler.removeIf(tWrapperHandler -> tWrapperHandler.getResourceId().equals(resourceId));
     }
 
     public void onComplete() {
         messagePublisher.onComplete();
-//        if (!messagePublisher.isEmpty())
-//            lastReceivedMessage.ifPresent(messagePublisher::onNext);
+//        wrapperHandler.decrementSubscribers();
+//        if (wrapperHandler.nonSubscribers())
+//            wrappersHandler.removeIf(tWrapperHandler -> tWrapperHandler.getResourceId().equals(resourceId));
     }
 }
+
+
+//package br.com.ubest.application.websocket;
+//
+//import reactor.core.publisher.UnicastProcessor;
+//
+//import java.util.List;
+//
+//public class WebSocketMessageSubscriber<T> {
+//
+//    private UnicastProcessor<T> messagePublisher;
+//
+//    WebSocketMessageSubscriber(final UnicastProcessor<T> messagePublisher) {
+//        this.messagePublisher = messagePublisher;
+//    }
+//
+//    void onNext(final T object) {
+//        messagePublisher.onNext(object);
+//    }
+//
+//    void onError(final Throwable error, final WrapperHandler<T> wrapperHandler, final List<WrapperHandler<T>> wrappersHandler, final Object resourceId) {
+//        error.printStackTrace();
+//        messagePublisher.onError(error);
+////        wrapperHandler.decrementSubscribers();
+////        if (wrapperHandler.nonSubscribers())
+////            wrappersHandler.removeIf(tWrapperHandler -> tWrapperHandler.getResourceId().equals(resourceId));
+//    }
+//
+//    public void onComplete(final WrapperHandler<T> wrapperHandler, final List<WrapperHandler<T>> wrappersHandler, final Object resourceId) {
+//        messagePublisher.onComplete();
+////        wrapperHandler.decrementSubscribers();
+////        if (wrapperHandler.nonSubscribers())
+////            wrappersHandler.removeIf(tWrapperHandler -> tWrapperHandler.getResourceId().equals(resourceId));
+//    }
+//}
