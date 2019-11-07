7d0e87182c33e4cdf45065c26437f393cd8a464e
Emanuel Victor <emanuel.fonseca@pti.org.br>
2019-11-07 18:22:23 -0300

    Handler de cupom funcionando, implementar handler de cartão de crédito!

--- back-end/src/main/java/br/com/ubest/application/resource/FaturaResource.java
+++ back-end/src/main/java/br/com/ubest/application/resource/FaturaResource.java
@@ -1,9 +1,13 @@
 package br.com.ubest.application.resource;
 
+import br.com.ubest.domain.entity.assinatura.Assinatura;
+import br.com.ubest.domain.entity.assinatura.Cupom;
 import br.com.ubest.domain.entity.assinatura.fatura.Fatura;
+import br.com.ubest.domain.entity.assinatura.fatura.Item;
 import br.com.ubest.domain.service.FaturaService;
 import br.com.ubest.infrastructure.resource.AbstractResource;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static br.com.ubest.domain.entity.usuario.Perfil.ADMINISTRADOR_VALUE;
 
@@ -51,7 +59,10 @@ public class FaturaResource extends AbstractResource<Fatura> {
     @PreAuthorize("hasAnyAuthority('" + ADMINISTRADOR_VALUE + "')")
     Mono<Optional<Fatura>> findById(@PathVariable final long id) {
         final Optional<Fatura> fatura = faturaService.findById(id);
-//        fatura.orElseThrow().getItens().forEach(item -> item.getFatura().getItens().forEach(item1 -> item1.setFatura(null)));
+        fatura.orElseThrow().getItens().forEach(item -> item.getFatura().getItens().forEach(item1 -> {
+            final Fatura fatura1 = new Fatura(fatura.get().getTenant(), fatura.get().getAssinatura(), fatura.get().getCupom(), null);
+            item1.setFatura(fatura1);
+        }));
         return Mono.just(fatura);
     }
 
