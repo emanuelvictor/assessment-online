3af05351171e986053df6e911fe4fe867c7de446
Emanuel Victor <emanuel.fonseca@pti.org.br>
2020-01-20 16:01:01 -0300

    fix #146

--- back-end/src/main/java/online/meavalia/application/resource/PlanoResource.java
+++ back-end/src/main/java/online/meavalia/application/resource/PlanoResource.java
@@ -2,10 +2,8 @@ package online.meavalia.application.resource;
 
 import lombok.RequiredArgsConstructor;
 import online.meavalia.domain.entity.assinatura.Plano;
-import online.meavalia.domain.entity.usuario.Perfil;
 import online.meavalia.domain.repository.PlanoRepository;
 import online.meavalia.infrastructure.resource.AbstractResource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +20,9 @@ public class PlanoResource extends AbstractResource<Plano> {
     private final PlanoRepository planoRepository;
 
     /**
-     *
      * @return
      */
     @GetMapping
-    @PreAuthorize("hasAnyAuthority('" + Perfil.ADMINISTRADOR_VALUE + "')")
     Flux<Plano> findAll() {
         return Flux.fromStream(this.planoRepository.findAll().stream());
     }
